fe20669f34fd08cfd0c2ff50799163cc4fbe4734
sakthi <sakthisiva@gmail.com>
2026-07-03 19:18:15 +0530

    added by sub2

--- 02.py
+++ 02.py
@@ -1,8 +1,9 @@
 a=int(input("Enter a number: "))
 b=int(input("Enter another number: "))
-c=a+b
-print("The sum of", a, "and", b, "is", c)
-if a > b:
-    print(a, "is greater than", b)
+c=int(input("Enter a third number: "))
+if a > b and a > c:
+    print(a, "is greater than", b, "and", c)
+elif b > a and b > c:
+    print(b, "is greater than", a, "and", c)
 else:
-    print(b, "is greater than", a)
+    print(c, "is greater than", a, "and", b)
